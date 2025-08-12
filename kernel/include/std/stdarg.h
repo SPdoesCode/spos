27ba725d3ab168d830bd96f9174fd68074a4f393
sp <lolsp.two@gmail.com>
2025-08-12 19:29:26 -0400

    Add files via upload

--- kernel/include/std/stdarg.h
+++ kernel/include/std/stdarg.h
@@ -9,10 +9,15 @@
 
 #include <std/stddef.h>
 
-typedef char* va_list;
+typedef unsigned char* va_list;
 
-#define va_start(ap, last_fixed_arg) (ap = (char*)&last_fixed_arg + sizeof(last_fixed_arg))
-#define va_arg(ap, type) (*(type*)((ap += sizeof(type)) - sizeof(type)))
-#define va_end(ap) (ap = NULL)
+#define _VA_ALIGN(type) (((sizeof(type) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+
+#define va_start(ap, last) (ap = (char*)&last + _VA_ALIGN(last))
+
+#define va_arg(ap, type) (*(type*)((ap += _VA_ALIGN(type)) - _VA_ALIGN(type)))
+
+#define va_end(ap) (ap = (va_list)0)
+
 
 #endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -20,14 +20,30 @@ void kmain(void) {
     serialprintln("Found device(s):");
     serialprintln("#. <device id> : <vendor id> : <class code> : <slot> : <subclass> : <prog if>");
     for (size_t i = 0; i < pci_entry_num; i++) {
-        serialprintln("%d. %s : %s : %s : %s : %s : %s", i, itoa(pcientrys[i].device_id), itoa(pcientrys[i].vendor_id), pciclasstostring(pcientrys[i].class_code), itoa((uint16_t)pcientrys[i].slot), itoa((uint16_t)pcientrys[i].subclass), itoa((uint16_t)pcientrys[i].prog_if));
+        serialprintln("%d. %d : %d : %s : %d : %d : %d",
+            i,
+            pcientrys[i].device_id,
+            pcientrys[i].vendor_id,
+            pciclasstostring(pcientrys[i].class_code),
+            pcientrys[i].slot,
+            pcientrys[i].subclass,
+            pcientrys[i].prog_if
+        );
     }
 
     txtprintln(0x0F, "Found ram (kb): %d", (total_memory_bytes / 1024));
     txtprintln(0x0F, "Found device(s): ");
     txtprintln(0x0F, "#. <device id> : <vendor id> : <class code> : <slot> : <subclass> : <prog if>");
     for (size_t i = 0; i < pci_entry_num; i++) {
-        txtprintln(0x0F, "%d. %s : %s : %s : %s : %s : %s", i, itoa(pcientrys[i].device_id), itoa(pcientrys[i].vendor_id), pciclasstostring(pcientrys[i].class_code), itoa((uint16_t)pcientrys[i].slot), itoa((uint16_t)pcientrys[i].subclass), itoa((uint16_t)pcientrys[i].prog_if));
+        txtprintln(0x0F, "%d. %d : %d : %s : %d : %d : %d",
+            i,
+            pcientrys[i].device_id,
+            pcientrys[i].vendor_id,
+            pciclasstostring(pcientrys[i].class_code),
+            pcientrys[i].slot,
+            pcientrys[i].subclass,
+            pcientrys[i].prog_if
+        );
     }
 
     halt();
